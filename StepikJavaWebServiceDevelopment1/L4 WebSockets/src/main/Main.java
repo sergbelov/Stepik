967f181694114334083444e6c526017aa432a607
Sergey Belov <sabelov@rambler.ru>
2018-03-24 19:07:20 +0300

    StepikJavaWebServiceDevelopment1 L4

---
+++ StepikJavaWebServiceDevelopment1/L4 WebSockets/src/main/Main.java
@@ -0,0 +1,42 @@
+package main;
+
+import chat.WebSocketChatServlet;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.util.logging.Logger;
+
+/**
+ * @author v.chibrikov
+ *         <p/>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p/>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+public class Main {
+
+    public static final Logger LOG = Logger.getGlobal(); // !!!
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
+        resource_handler.setResourceBase("public_html");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+        server.setHandler(handlers);
+
+        server.start();
+        LOG.info("Server started"); // !!!
+        server.join();
+    }
+}
